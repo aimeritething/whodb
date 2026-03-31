47227203c4ebd1ae6911c40a7dcdddeedfc6fb3e
aimeritething <aimeritething@gmail.com>
2026-03-31 19:52:01 +0800

    feat(i18n): localize mongodb views and document flows

--- dataflow/src/i18n/locales/en/index.ts
+++ dataflow/src/i18n/locales/en/index.ts
@@ -1,11 +1,13 @@
 import { enCommonMessages } from './common'
 import { enLayoutMessages } from './layout'
+import { enMongodbMessages } from './mongodb'
 import { enSqlMessages } from './sql'
 import { enSidebarMessages } from './sidebar'
 
 export const enMessages = {
   ...enCommonMessages,
   ...enLayoutMessages,
+  ...enMongodbMessages,
   ...enSqlMessages,
   ...enSidebarMessages,
 } as const
--- dataflow/src/i18n/locales/zh/index.ts
+++ dataflow/src/i18n/locales/zh/index.ts
@@ -1,11 +1,13 @@
 import { zhCommonMessages } from './common'
 import { zhLayoutMessages } from './layout'
+import { zhMongodbMessages } from './mongodb'
 import { zhSqlMessages } from './sql'
 import { zhSidebarMessages } from './sidebar'
 
 export const zhMessages = {
   ...zhCommonMessages,
   ...zhLayoutMessages,
+  ...zhMongodbMessages,
   ...zhSqlMessages,
   ...zhSidebarMessages,
 } as const
